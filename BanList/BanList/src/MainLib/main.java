306db1ed99716ec46c251e06ed311d5f1399476c
Aviloo <106913246+Aviloo@users.noreply.github.com>
2022-07-23 14:17:12 +0300

    BanList
    Save banned players to mysql

---
+++ BanList/BanList/src/MainLib/main.java
@@ -0,0 +1,72 @@
+package MainLib;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import commands.suspect;
+import commands.unsuspect;
+
+public class main extends JavaPlugin implements Listener {
+	private Connection connection;
+	public String host, database, username, password, table;
+	public int port;
+
+	public void onEnable() {
+		loadConfig();
+		mysqlSetup();
+		Bukkit.getServer().getPluginManager().registerEvents(new BanHandler(), this);
+		//Bukkit.getServer().getPluginCommand("suspect").setExecutor(new suspect());
+		//Bukkit.getServer().getPluginCommand("unsuspect").setExecutor(new unsuspect());
+
+	}
+	public void loadConfig(){
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+	public void mysqlSetup(){
+		host = this.getConfig().getString("host");
+		port = this.getConfig().getInt("port");
+		database = this.getConfig().getString("database");
+		username = this.getConfig().getString("username");
+		password = this.getConfig().getString("password");
+		table = this.getConfig().getString("table");
+		
+		try{
+			
+			synchronized (this){
+				if(getConnection() != null && !getConnection().isClosed()){
+					return;
+				}
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				setConnection( DriverManager.getConnection("jdbc:mysql://" + this.host + ":" 
+				+ this.port + "/" + this.database, this.username, this.password));
+				
+				Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "MYSQL CONNECTED");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public void OnDisable() {
+		
+	}
+
+}
